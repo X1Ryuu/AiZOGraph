3e2a395f47d65fc63a59ce04b395d8b91618291e
Bartłomiej <krupaczek.b@gmail.com>
2025-05-21 22:12:30 +0200

    Force

---
+++ cmake-build-debug/CMakeFiles/AiZOGraph.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Bart/GitVC/AiZOGraph/Algorithms/Bellman/Bellman.cpp" "CMakeFiles/AiZOGraph.dir/Algorithms/Bellman/Bellman.cpp.obj" "gcc" "CMakeFiles/AiZOGraph.dir/Algorithms/Bellman/Bellman.cpp.obj.d"
+  "C:/Users/Bart/GitVC/AiZOGraph/Algorithms/Dijkstra/Dijkstra.cpp" "CMakeFiles/AiZOGraph.dir/Algorithms/Dijkstra/Dijkstra.cpp.obj" "gcc" "CMakeFiles/AiZOGraph.dir/Algorithms/Dijkstra/Dijkstra.cpp.obj.d"
+  "C:/Users/Bart/GitVC/AiZOGraph/Algorithms/Kruskal/Kruskal.cpp" "CMakeFiles/AiZOGraph.dir/Algorithms/Kruskal/Kruskal.cpp.obj" "gcc" "CMakeFiles/AiZOGraph.dir/Algorithms/Kruskal/Kruskal.cpp.obj.d"
+  "C:/Users/Bart/GitVC/AiZOGraph/Algorithms/Prim/Prim.cpp" "CMakeFiles/AiZOGraph.dir/Algorithms/Prim/Prim.cpp.obj" "gcc" "CMakeFiles/AiZOGraph.dir/Algorithms/Prim/Prim.cpp.obj.d"
+  "C:/Users/Bart/GitVC/AiZOGraph/List/GraphList.cpp" "CMakeFiles/AiZOGraph.dir/List/GraphList.cpp.obj" "gcc" "CMakeFiles/AiZOGraph.dir/List/GraphList.cpp.obj.d"
+  "C:/Users/Bart/GitVC/AiZOGraph/Matrix/GraphIncidence.cpp" "CMakeFiles/AiZOGraph.dir/Matrix/GraphIncidence.cpp.obj" "gcc" "CMakeFiles/AiZOGraph.dir/Matrix/GraphIncidence.cpp.obj.d"
+  "C:/Users/Bart/GitVC/AiZOGraph/Utils/utils.cpp" "CMakeFiles/AiZOGraph.dir/Utils/utils.cpp.obj" "gcc" "CMakeFiles/AiZOGraph.dir/Utils/utils.cpp.obj.d"
+  "C:/Users/Bart/GitVC/AiZOGraph/main.cpp" "CMakeFiles/AiZOGraph.dir/main.cpp.obj" "gcc" "CMakeFiles/AiZOGraph.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/AiZOGraph.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "AiZOGraph.exe"
+  "AiZOGraph.exe.manifest"
+  "AiZOGraph.pdb"
+  "CMakeFiles/AiZOGraph.dir/Algorithms/Bellman/Bellman.cpp.obj"
+  "CMakeFiles/AiZOGraph.dir/Algorithms/Bellman/Bellman.cpp.obj.d"
+  "CMakeFiles/AiZOGraph.dir/Algorithms/Dijkstra/Dijkstra.cpp.obj"
+  "CMakeFiles/AiZOGraph.dir/Algorithms/Dijkstra/Dijkstra.cpp.obj.d"
+  "CMakeFiles/AiZOGraph.dir/Algorithms/Kruskal/Kruskal.cpp.obj"
+  "CMakeFiles/AiZOGraph.dir/Algorithms/Kruskal/Kruskal.cpp.obj.d"
+  "CMakeFiles/AiZOGraph.dir/Algorithms/Prim/Prim.cpp.obj"
+  "CMakeFiles/AiZOGraph.dir/Algorithms/Prim/Prim.cpp.obj.d"
+  "CMakeFiles/AiZOGraph.dir/List/GraphList.cpp.obj"
+  "CMakeFiles/AiZOGraph.dir/List/GraphList.cpp.obj.d"
+  "CMakeFiles/AiZOGraph.dir/Matrix/GraphIncidence.cpp.obj"
+  "CMakeFiles/AiZOGraph.dir/Matrix/GraphIncidence.cpp.obj.d"
+  "CMakeFiles/AiZOGraph.dir/Utils/utils.cpp.obj"
+  "CMakeFiles/AiZOGraph.dir/Utils/utils.cpp.obj.d"
+  "CMakeFiles/AiZOGraph.dir/main.cpp.obj"
+  "CMakeFiles/AiZOGraph.dir/main.cpp.obj.d"
+  "libAiZOGraph.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AiZOGraph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
